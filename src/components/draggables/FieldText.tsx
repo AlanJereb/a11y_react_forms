b50e2b3ea249bf509bfd2c142fcd8fd60963af15
Alan Jereb <alanjereb@gmail.com>
2025-10-16 22:22:36 +0200

    feat: swap editor droptarget with multiple droptargets, set editor elements to app's context - WIP

--- src/components/draggables/FieldText.tsx
+++ src/components/draggables/FieldText.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { DraggableItem, DraggableState } from "../../types/types.js";
+import type { DraggableItem } from "../../types/types.js";
 import FieldBase from "../form/FieldBase.js";
 
 const FieldText = (props: DraggableItem) => {
@@ -10,7 +10,7 @@ const FieldText = (props: DraggableItem) => {
       <input
         type="text"
         placeholder="Enter title"
-        className="bg-background w-full rounded-[1rem] border-none p-[1.2rem] text-[1.4rem] font-medium inset-shadow-[-5px_-5px_10px_0_#FFFFFF70,5px_5px_10px_0_#AEAEC020] outline-none"
+        className="bg-background w-full rounded-[1rem] border-none p-[1.2rem] hover:cursor-grab text-[1.4rem] font-medium inset-shadow-[-5px_-5px_10px_0_#FFFFFF70,5px_5px_10px_0_#AEAEC020] outline-none"
         disabled={disabled}
       />
     </FieldBase>
--- src/components/form/FieldBase.tsx
+++ src/components/form/FieldBase.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   draggable
 } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
@@ -15,14 +15,23 @@ const FieldBase: React.FC<FieldBaseProps> = ({
   fieldType,
   label,
   children,
+  index,
 }) => {
   const ref = useRef<HTMLDivElement | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
 
   useEffect(() => {
     if (!ref.current) return;
     const cleanup = combine(
       draggable({
         element: ref.current!,
+        onDragStart: () => {
+          // TODO: modify add drop targets between editor elements, between all elements,
+          // but the element on current index and one behind and one in front of it
+          // TODO: move editor state to the app context, so that you can access from both sidebar and editor
+          setIsDragging(true)
+        },
+        onDrop: () => setIsDragging(false),
         getInitialData: () => ({
           type: Constants.fieldTypeCard,
           id: id,
@@ -34,7 +43,7 @@ const FieldBase: React.FC<FieldBaseProps> = ({
   }, [id]);
 
   return (
-    <div ref={ref} className="p-[1.5rem]">
+    <div ref={ref} className={`p-[1.5rem] hover:cursor-grab ${isDragging ? "opacity-40" : ""}`}>
       {label && <p className="text-start text-[1rem] font-medium">{label}</p>}
       {children}
     </div>
